c015ffc8b4a6a752c49d519402bc97bd82af25d2
Ashraf Mohammed <21951A6612@iare.ac.in>
2023-07-10 22:12:40 +0530

    change font size of footer

--- script/index.js
+++ script/index.js
@@ -1,22 +1,21 @@
 const express = require('express');
-const moment = require('moment');
+const app = express();
 
+const moment = require('moment');
 const bodyParser = require('body-parser');
 const path = require('path');
 const port = process.env.PORT || 8080;
-
-const app = express();
-
-const worksheetRoute = require('./controllers/worksheet');
-const specificRoute = require('./controllers/specific');
-const bulkRoute = require('./controllers/bulk');
-const statusRoute = require('./controllers/status');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+const worksheetRoute = require('./controllers/worksheet');
+const specificRoute = require('./controllers/specific');
+const bulkRoute = require('./controllers/bulk');
+const statusRoute = require('./controllers/status');
 
 //Home Page
 app.get('/', (req, res) => {
